2089085f421837ed8bf17e507ea26417558837e1
javajigi <javajigi@gmail.com>
2018-06-19 15:52:10 +0900

    initial commit

---
+++ src/main/java/codesquad/BaeminchanApplication.java
@@ -0,0 +1,11 @@
+package codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeminchanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaeminchanApplication.class, args);
+    }
+}
