314af3ce1cebb77f1b0899aba796e385ffb5ebd0
mia6111 <mhyun1116@gmail.com>
2018-07-24 18:57:30 +0900

    Add Login 기능 및 테스트 구현
    - login 성공, 실페 ATDD 구현
    - Controller Login - session 저장 및 JsonResponse 리턴

---
+++ src/main/java/codesquad/util/SessionUtil.java
@@ -0,0 +1,9 @@
+package codesquad.util;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+public class SessionUtil {
+    public static final String SESSION_KEY = "EMAIL_SESSION_KEY";
+
+}
