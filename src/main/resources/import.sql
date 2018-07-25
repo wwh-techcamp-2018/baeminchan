d2a755f3f8fb830f6a9f9454a473ce0edca55164
sheep1500 <woowatechcamp_17@woowatechcamp17.local>
2018-07-25 18:47:24 +0900

    database 설정 및 user data 추가 - h2-console 설정 - user sample data 추가

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (id, email, name, password, phone, deleted) values (1, 'sample@woowahan.com', 'james', '123456a!', '010-1234-1234', false);
+INSERT INTO user (id, email, name, password, phone, deleted) values (2, 'sample2@woowahan.com', 'serena', '123456b!', '010-5678-5678', false);
