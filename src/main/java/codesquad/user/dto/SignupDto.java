24af977a1a56ceaff735c70f59ebe75292b3ecb2
Nooheat <nooheat1228@gmail.com>
2018-07-25 15:19:24 +0900

    refactor: Enable using messages.properties
    - removed validation messages from SignupDto (move to messages.properties)

--- src/main/java/codesquad/user/dto/SignupDto.java
+++ src/main/java/codesquad/user/dto/SignupDto.java
@@ -21,26 +21,23 @@ import javax.validation.constraints.Size;
 @Setter
 public class SignupDto {
 
-    @NotNull(message = "이메일을 입력해주세요.")
-    @Email(message = "이메일 형식에 맞게 입력해주세요.",
-            regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
+    @NotNull
+    @Email(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
             flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
 
-    @NotNull(message = "비밀번호를 입력해주세요.")
-    @Pattern(message = "영문 및 숫자를 포함한 8자 이상 16자 이하로 입력해주세요.",
-            regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z]).*$")
+    @NotNull
+    @Pattern(regexp = "^.*(?=^.{8,16}$)(?=.*\\d)(?=.*[a-zA-Z]).*$")
     private String password;
 
     private String confirmPassword;
 
-    @NotNull(message = "이름을 입력해주세요.")
-    @Size(message = "2자 이상 45자 이하로 입력해주세요.", min = 2, max = 45)
+    @NotNull
+    @Size(min = 2, max = 45)
     private String name;
 
-    @NotNull(message = "전화번호를 입력해주세요.")
-    @Pattern(message = "전화번호를 000-0000-0000 형태로 입력해주세요.",
-            regexp = "^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$")
+    @NotNull
+    @Pattern(regexp = "^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$")
     private String phoneNumber;
 
     public SignupDto(String email, String password, String confirmPassword, String name, String phoneNumber) {
