c6129d2ca1aac396f3a6ab25f1450a96a3963d28
Mi Hyon <mhyun1116@gmail.com>
2018-07-27 16:05:28 +0900

    Add ValidationError 에러 처리 클래스 추가
    - 필드 에러 JSON응답용 ValidationError 클래스 추가
    - 테스트 코드에서 에러 확인
    - static 파일 자동 반영하도록 설정

--- src/main/java/codesquad/validate/ExceptionControllerAdvice.java
+++ src/main/java/codesquad/validate/ExceptionControllerAdvice.java
@@ -1,11 +1,8 @@
 package codesquad.validate;
 
-import codesquad.support.ErrorResponse;
-import codesquad.support.JsonResponse;
-import codesquad.support.NotExistException;
+import codesquad.support.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -14,16 +11,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.annotation.Resource;
 import java.util.List;
-import java.util.Optional;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
     private static final Logger log = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
 
-//    @Resource(name = "messageSourceAccessor")
-//    private MessageSourceAccessor msa;
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -35,7 +28,8 @@ public class ExceptionControllerAdvice {
         for (ObjectError objectError : errors) {
             log.debug("object error : {}", objectError);
             FieldError fieldError = (FieldError) objectError;
-            errorResponse.registErrorMessage(fieldError.getField() + fieldError.getDefaultMessage());
+            //log.debug("FieldError {}", fieldError);
+            errorResponse.registErrorMessage(new ValidationError(fieldError.getDefaultMessage(), fieldError.getField()));
         }
         return errorResponse;
     }
@@ -43,7 +37,7 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler( {IllegalArgumentException.class, NotExistException.class} )
     public ErrorResponse handleIllegalArgumentException(RuntimeException exception){
         ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.registErrorMessage(exception.getMessage());
+        errorResponse.registErrorMessage(new ValidationError(exception.getMessage()));
         return errorResponse;
     }
 }
