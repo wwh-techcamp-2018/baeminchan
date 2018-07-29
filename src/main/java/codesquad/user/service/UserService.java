35d25119bad29f88b34ec00da77f2e57f190f280
hyukjin-lee <gogokow27@gmail.com>
2018-07-29 15:42:42 +0900

    ### domain 추가 및 수정 - Role 클래스 재정의 - User 클래스에 Role 적용 - LoginDto, UserDto 추가 - UserService 추가

---
+++ src/main/java/codesquad/user/service/UserService.java
@@ -0,0 +1,51 @@
+package codesquad.user.service;
+
+import codesquad.user.domain.User;
+import codesquad.user.domain.UserRepository;
+import codesquad.user.dto.LoginDto;
+import codesquad.user.dto.UserDto;
+import codesquad.exception.UnAuthenticationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Service
+public class UserService {
+
+    public static final String USER_SESSION_KEY = "loginId";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public User save(UserDto newUserDto) {
+        User newUser = User.builder()
+                .email(newUserDto.getEmail())
+                .name(newUserDto.getName())
+                .password(encoder.encode(newUserDto.getPassword()))
+                .phone(newUserDto.getPhone())
+                .build();
+
+        log.debug("body: {}", newUser);
+        return userRepository.save(newUser);
+    }
+
+    public User login(LoginDto loginDto) {
+        User maybeUser = userRepository.findByEmail(loginDto.getEmail()).orElseThrow(UnAuthenticationException::invalidEmail);
+        if (!encoder.matches(loginDto.getPassword(), maybeUser.getPassword())) {
+            throw UnAuthenticationException.invalidPassword();
+        }
+        log.info("-------------------user: {}", maybeUser);
+        return maybeUser;
+    }
+
+    public void logout(HttpSession httpSession) {
+        httpSession.removeAttribute(USER_SESSION_KEY);
+    }
+}
