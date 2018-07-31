f4bc9c5abcd40e35c0832fdf0d563f0514398ecd
YeonTaeKIM <urbanscenery@gmail.com>
2018-07-31 12:28:59 +0900

    Add 메뉴 더미데이터

---
+++ src/main/resources/import.sql
@@ -0,0 +1,60 @@
+insert into menu (name, child_count, index) values ('menu_root', 0, 0);
+
+insert into menu (name, child_count, index, parent_id) values ('밑반찬', 8, 1, 1);
+insert into menu (name, child_count, index, parent_id) values ('국찌개', 4, 2, 1);
+insert into menu (name, child_count, index, parent_id) values ('메인반찬', 4, 3, 1);
+insert into menu (name, child_count, index, parent_id) values ('아이반찬', 4, 4, 1);
+insert into menu (name, child_count, index, parent_id) values ('정기식단', 4, 5, 1);
+insert into menu (name, child_count, index, parent_id) values ('신선가공', 4, 6, 1);
+insert into menu (name, child_count, index, parent_id) values ('간식', 4, 7, 1);
+insert into menu (name, child_count, index, parent_id) values ('브랜드관', 4, 8, 1);
+
+insert into menu (name, child_count, index, parent_id) values ('무침', 0, 1, 2);
+insert into menu (name, child_count, index, parent_id) values ('맛있는무침', 0, 2, 2);
+insert into menu (name, child_count, index, parent_id) values ('맛없는무침', 0, 3, 2);
+insert into menu (name, child_count, index, parent_id) values ('짠무침', 0, 4, 2);
+insert into menu (name, child_count, index, parent_id) values ('싱거운무침', 0, 5, 2);
+insert into menu (name, child_count, index, parent_id) values ('매운무침', 0, 6, 2);
+insert into menu (name, child_count, index, parent_id) values ('뿌링클무침', 0, 7, 2);
+insert into menu (name, child_count, index, parent_id) values ('몸에좋은무침', 0, 8, 2);
+
+
+insert into menu (name, child_count, index, parent_id) values ('33무침', 0, 1, 3);
+insert into menu (name, child_count, index, parent_id) values ('33맛있는무침', 0, 2, 3);
+insert into menu (name, child_count, index, parent_id) values ('33맛없는무침', 0, 3, 3);
+insert into menu (name, child_count, index, parent_id) values ('33짠무침', 0, 4, 3);
+
+
+insert into menu (name, child_count, index, parent_id) values ('44무침', 0, 1, 4);
+insert into menu (name, child_count, index, parent_id) values ('44맛있는무침', 0, 2, 4);
+insert into menu (name, child_count, index, parent_id) values ('44맛없는무침', 0, 3, 4);
+insert into menu (name, child_count, index, parent_id) values ('44짠무침', 0, 4, 4);
+
+
+insert into menu (name, child_count, index, parent_id) values ('55무침', 0, 1, 5);
+insert into menu (name, child_count, index, parent_id) values ('55맛있는무침', 0, 2, 5);
+insert into menu (name, child_count, index, parent_id) values ('55맛없는무침', 0, 3, 5);
+insert into menu (name, child_count, index, parent_id) values ('55짠무침', 0, 4, 5);
+
+
+insert into menu (name, child_count, index, parent_id) values ('66무침', 0, 1, 6);
+insert into menu (name, child_count, index, parent_id) values ('66맛있는무침', 0, 2, 6);
+insert into menu (name, child_count, index, parent_id) values ('66맛없는무침', 0, 3, 6);
+insert into menu (name, child_count, index, parent_id) values ('66짠무침', 0, 4, 6);
+
+insert into menu (name, child_count, index, parent_id) values ('77무침', 0, 1, 7);
+insert into menu (name, child_count, index, parent_id) values ('77맛있는무침', 0, 2, 7);
+insert into menu (name, child_count, index, parent_id) values ('77맛없는무침', 0, 3, 7);
+insert into menu (name, child_count, index, parent_id) values ('77짠무침', 0, 4, 7);
+
+insert into menu (name, child_count, index, parent_id) values ('88무침', 0, 1, 8);
+insert into menu (name, child_count, index, parent_id) values ('88맛있는무침', 0, 2, 8);
+insert into menu (name, child_count, index, parent_id) values ('88맛없는무침', 0, 3, 8);
+insert into menu (name, child_count, index, parent_id) values ('88짠무침', 0, 4, 8);
+
+insert into menu (name, child_count, index, parent_id) values ('99무침', 0, 1, 9);
+insert into menu (name, child_count, index, parent_id) values ('99맛있는무침', 0, 2, 9);
+insert into menu (name, child_count, index, parent_id) values ('99맛없는무침', 0, 3, 9);
+insert into menu (name, child_count, index, parent_id) values ('99짠무침', 0, 4, 9);
+
+
