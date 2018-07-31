02c6c8ff75459a3a6da1cb20765039b7d24cb3f8
YeonTaeKIM <urbanscenery@gmail.com>
2018-07-31 19:43:32 +0900

    Add 특정 메뉴 가져오는 Api / Test
    - MenuRepository 에 최상단 메뉴 select 메소드 변경
    - findBy [column] IsNull()

--- src/main/java/codesquad/web/ApiMenuController.java
+++ src/main/java/codesquad/web/ApiMenuController.java
@@ -2,9 +2,10 @@ package codesquad.web;
 
 import codesquad.domain.Menu;
 import codesquad.repository.MenuRepository;
-import org.apache.commons.lang3.builder.ToStringExclude;
+import codesquad.support.NotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class ApiMenuController {
 
     @GetMapping("")
     public Menu allMenu(){
-        return menuRepository.findById((long)1).get();
+        return menuRepository.findByParentIsNull().orElseThrow(() -> new NotExistException("최상단 메뉴 존재하지 않음"));
+    }
+
+    @GetMapping("/{id}")
+    public Menu specificMenu(@PathVariable long id){
+        return menuRepository.findById(id).orElseThrow(() -> new NotExistException("없는 메뉴 입니다."));
     }
 }
--- src/test/java/codesquad/atdd/ApiMenuAcceptanceTest.java
+++ src/test/java/codesquad/atdd/ApiMenuAcceptanceTest.java
@@ -1,10 +1,12 @@
 package codesquad.atdd;
 
 import codesquad.domain.Menu;
+import codesquad.repository.MenuRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
@@ -18,15 +20,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ApiMenuAcceptanceTest extends AcceptanceTest{
     private static final Logger log = LoggerFactory.getLogger(ApiMenuAcceptanceTest.class);
 
-    @Before
-    public void setUp(){
-
-    }
+    @Autowired
+    MenuRepository menuRepository;
 
     @Test
     public void getMenuAll_성공() throws URISyntaxException {
         RequestEntity<Void> requestEntity = RequestEntity.get(new URI("/api/menu")).accept(MediaType.APPLICATION_JSON).build();
         ResponseEntity<Menu> responseEntity = template().exchange(requestEntity, Menu.class);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getParent()).isNull();
+    }
+
+    @Test
+    public void get단일메뉴_성공() throws URISyntaxException {
+        Menu menu = menuRepository.findByParentIsNull().get();
+
+        RequestEntity<Void> requestEntity = RequestEntity.get(new URI("/api/menu/"+menu.getId())).accept(MediaType.APPLICATION_JSON).build();
+        ResponseEntity<Menu> responseEntity = template().exchange(requestEntity, Menu.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getId()).isEqualTo(menu.getId());
     }
 }
