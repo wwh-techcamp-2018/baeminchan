64dcb3819347274f5bc6554daabbbee47dd84c59
ffaass <rlatnghd95@gmail.com>
2018-07-31 10:26:52 +0900

    feat: Create category frontend
    - Add ajax request for main, sub categories

---
+++ src/main/resources/static/js/category.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadMainCatetories();
+});
+
+function loadMainCatetories() {
+    new FetchManager({
+        url: '/api/categories',
+        method: 'GET'
+    })
+    .onSuccess(response => {
+        response.data.forEach(mainCategory => {
+            createMainCategoryElement(mainCategory);
+        });
+    })
+    .onFailed(errors => {
+        
+    })
+    .fetch();
+}
+
+function createMainCategoryElement(mainCategory) {
+    const mainCategoryComponent = `
+        <li id="category-${mainCategory.id}" data-id="${mainCategory.id}">
+            <a href="#">${mainCategory.title}</a>
+            <ul class="sub-menu">
+            </ul>
+        </li>
+    `;
+    $('#gnb .inner-wrapper .menu').insertAdjacentHTML('beforeend', mainCategoryComponent);
+    addEvent(`#category-${mainCategory.id}`, 'mouseover', loadSubCategories);
+}
+
+function loadSubCategories(evt) {
+    let mainCategory = evt.target;
+
+    if (mainCategory.getAttribute('data-id') === null) {
+        mainCategory = evt.target.parentElement;
+    }
+
+    mainCategory.removeEventListener('mouseover', loadSubCategories);
+
+    new FetchManager({
+        url: `/api/categories/${mainCategory.getAttribute('data-id')}`,
+        method: 'GET'
+    })
+    .onSuccess(response => {
+        response.data.forEach(subCategory => {
+            createSubCategoryElement(subCategory, mainCategory);
+        })
+    })
+    .onFailed(errors => {
+        mainCategory.addEventListener('mouseover', loadSubCategories);  
+    })
+    .fetch();
+}
+
+function createSubCategoryElement(subCategory, mainCategory) {
+    const subCategoryComponent = `
+        <li>
+            <a href="#">${subCategory.title}</a>
+        </li>
+    `;
+    mainCategory.querySelector('.sub-menu').insertAdjacentHTML('beforeend', subCategoryComponent);
+}
