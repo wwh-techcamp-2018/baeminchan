6a76a299750dc58c5a646e22b4a0f32ddbd67323
YeonTaeKIM <urbanscenery@gmail.com>
2018-07-31 12:29:56 +0900

    Add 메뉴 프론트 연동

--- src/main/resources/static/js/constant.js
+++ src/main/resources/static/js/constant.js
@@ -1,10 +1,3 @@
-const METHOD = {
-    GET : 'GET',
-    POST : 'POST',
-    PUT : 'PUT',
-    DELETE : 'DELETE',
-}
-
 const ContentType = {
     JSON : {
         'content-type': 'application/json',
