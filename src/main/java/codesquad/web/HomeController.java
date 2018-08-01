702afbafad56412eedad092fcc85f6291822ebe8
Mi Hyon <mhyun1116@gmail.com>
2018-08-01 21:37:00 +0900

    Add 프로모션 데이터 서버에서 생성
    - 프로모션에 대한 정보를 핸들바로 랜더링해서 전송
    - 프로모션 이미지를 inline 처리
    - javascript 랑 css 를 index.html에서 분리
    - db에 프로모션 데이터 저장
    - "JPA엔티티", HomeController

---
+++ src/main/java/codesquad/web/HomeController.java
@@ -0,0 +1,23 @@
+package codesquad.web;
+
+import codesquad.domain.Promotion;
+import codesquad.repository.PromotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    PromotionRepository promotionRepository;
+
+    @GetMapping
+    public String welcomePage(Model model){
+        model.addAttribute("promotionList", promotionRepository.findAll());
+        return "/index";
+    }
+}
