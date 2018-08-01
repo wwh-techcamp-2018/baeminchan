fffb31843abf18ebf8a73e5cb42fe4f247f01b93
ffaass <rlatnghd95@gmail.com>
2018-08-01 14:12:27 +0900

    feat: Implement promotion server-side rendering
    - Add acceptance test
    - Add promotion dummy data
    - Add home view with dummy data
    - Fix html syntax errors

---
+++ src/test/java/codesquad/promotion/web/PromotionAcceptanceTest.java
@@ -0,0 +1,26 @@
+package codesquad.promotion.web;
+
+import codesquad.promotion.domain.Promotion;
+import codesquad.promotion.domain.PromotionRepository;
+import codesquad.support.AcceptanceTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PromotionAcceptanceTest extends AcceptanceTest {
+    @Autowired
+    private PromotionRepository promotionRepository;
+
+    @Test
+    public void getPromotionList() {
+        Promotion promotion = Promotion.builder().imgUrl("https://blah.co.kr/static/img/test.jpg").build();
+        promotionRepository.save(promotion);
+
+        ResponseEntity<String> responseEntity = template().getForEntity("/", String.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).contains(promotion.getImgUrl());
+    }
+}
