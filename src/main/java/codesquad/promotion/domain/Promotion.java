b662c82731c69611a8f078c9e9f13a60fdc3bdd6
Nooheat <nooheat1228@gmail.com>
2018-08-01 11:23:03 +0900

    feat: Create promotion entity
    - Create promotion entity with a test
    This proves that this test is working properly

--- src/main/java/codesquad/promotion/domain/Promotion.java
+++ src/main/java/codesquad/promotion/domain/Promotion.java
@@ -1,7 +1,15 @@
 package codesquad.promotion.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String imgUrl;
-    private Long order;
+
 }
