8676c2f68fb6b61e336b17d0be17c7da3d51d844
dain7672 <dain7672@gmail.com>
2018-08-01 18:08:23 +0900

    Add profile settings

--- src/test/java/codesquad/web/CategoryAcceptanceTest.java
+++ src/test/java/codesquad/web/CategoryAcceptanceTest.java
@@ -18,12 +18,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CategoryAcceptanceTest {
     public static final Logger log = LoggerFactory.getLogger(MemberAcceptanceTest.class);
     @Autowired
