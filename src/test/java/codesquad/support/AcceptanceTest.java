8b5f1be5076e9fbaa0f5881f4029faa0296f80e8
ffaass <rlatnghd95@gmail.com>
2018-08-01 18:20:52 +0900

    chore: Apply deployment environment settings
    - Add log rolling policy (prod)
    - Add development configuration

--- src/test/java/codesquad/support/AcceptanceTest.java
+++ src/test/java/codesquad/support/AcceptanceTest.java
@@ -11,12 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("development")
 public abstract class AcceptanceTest {
     @Autowired
     protected UserRepository userRepository;
