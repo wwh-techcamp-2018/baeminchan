a14ac92306a49c5069ce2132892078990e6297f5
minzzzy <minpjh@gmail.com>
2018-08-03 20:34:44 +0900

    feat: Add BestProduct Entity

--- src/main/java/codesquad/product/domain/BestProduct.java
+++ src/main/java/codesquad/product/domain/BestProduct.java
@@ -1,9 +1,20 @@
 package codesquad.product.domain;
 
+import lombok.Getter;
+
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Getter
 public class BestProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 30)
     private String name;
+
+    @OneToMany
     private List<Product> products;
 }
