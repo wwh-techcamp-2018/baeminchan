8befb004894283937c8940ac36a8d1011be2e070
doooyeon <ehdus5958@naver.com>
2018-08-06 19:49:45 +0900

    Apply cache on best category
    build.gradle : cache 의존성 추가
    BaeminchanApplication, BestCategoryService : cache 어노테이션 추가

--- src/main/java/codesquad/BaeminchanApplication.java
+++ src/main/java/codesquad/BaeminchanApplication.java
@@ -2,8 +2,10 @@ package codesquad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BaeminchanApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaeminchanApplication.class, args);
