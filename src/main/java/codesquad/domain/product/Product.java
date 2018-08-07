d5f45c682de887e7855242a68c5404b3d5372c5d
kwangilcho <kwangil.steve.cho@gmail.com>
2018-08-07 10:31:31 +0900

    [team6_pair2] Step4 피드백 적용하였습니다. (#65)
    * 베스트 반찬 백엔드와 프론트엔드(Tab ui) 구현 완료
    
    1. Backend
      - Create Product domain class
      - Create ApiProductController for list all products
      - Create product service & product repository
    
    2. Frontend
      - Create tab ui with best products
      - Initialize tab randomly
    
    * Enable cache for category, promotion, product
    
    1. add .gitignore to ignore logfiles
    
    * Add product field validation, apply reducer to best tab js

--- src/main/java/codesquad/domain/product/Product.java
+++ src/main/java/codesquad/domain/product/Product.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -13,9 +15,18 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min = 1, max = 255)
     private String title;
+
+    @Size(min = 1, max = 255)
     private String description;
+
+    @Size(min = 1, max = 255)
     private String imgUrl;
+
+    @DecimalMin(value = "0")
     private Long price;
+
     private String bestTab;
 }
