0aa2e3006ef255d9ab01c6c1c307c6598d3aa7f7
DongHeeYun <inter0960@naver.com>
2018-08-07 10:01:56 +0900

    피드백 반영 (#64)
    1. classList.toggle을 add, remove로 변경하여 의미를 구체화함
    2. 메소드명 변경 (*Html* -> *HTML*)
    3. onRandomTab에서 하드코딩한 값을 계산해서 구하도록 변경

--- src/main/resources/static/js/best.js
+++ src/main/resources/static/js/best.js
@@ -17,7 +17,7 @@ function getBestMenu() {
 function addBestMenuUi(result) {
     result.json().then(menus => {
         menus.forEach((menu,i) => {
-            $(".tab-btn-box").insertAdjacentHTML("beforeend", makeHtmlMenuBtn(menu,i));
+            $(".tab-btn-box").insertAdjacentHTML("beforeend", makeHTMLMenuBtn(menu,i));
             $(".tab-content-group-box").insertAdjacentHTML("beforeend", makeHtmlMenuTab(menu.products));
         })
         onRandomTab();
@@ -29,14 +29,14 @@ function makeHtmlMenuTab(products) {
                         <ul class="tab-content-box">
                 `;
     products.forEach(product =>{
-        tabHtml += makeHtmlMenu(product)
+        tabHtml += makeHTMLMenu(product)
     })
     tabHtml += `    </ul>
                 </li>`
     return tabHtml;
 }
 
-function makeHtmlMenu(product) {
+function makeHTMLMenu(product) {
     const price = Number(product.price).toLocaleString('en');
     const html = `
                      <li>
@@ -62,7 +62,7 @@ function makeHtmlMenu(product) {
 }
 
 
-function makeHtmlMenuBtn(menu,index) {
+function makeHTMLMenuBtn(menu, index) {
     const html = `  <li data-index="${index}">
                     <a data-category-id="&{menu.id}" href="#">${menu.title}</a>
                 </li>`
@@ -71,9 +71,10 @@ function makeHtmlMenuBtn(menu,index) {
 
 
 function onRandomTab() {
-    const randomValue = Math.floor(Math.random() * 6);
+    const btnBox = $(".tab-btn-box").children;
+    const randomValue = Math.floor(Math.random() * btnBox.length);
     $(".tab-content-group-box").children[randomValue].classList.add("on");
-    $(".tab-btn-box").children[randomValue].classList.add("on");
+    btnBox[randomValue].classList.add("on");
 }
 
 function tabBtnHandler(evt) {
@@ -83,11 +84,11 @@ function tabBtnHandler(evt) {
 }
 
 function onTab(index) {
-    $(".tab-btn-box").children[index].classList.toggle("on");
+    $(".tab-btn-box").children[index].classList.add("on");
     $(".tab-content-group-box").children[index].classList.add("on");
 }
 
 function offTab() {
-    $(".tab-btn-box .on").classList.toggle("on");
-    $(".tab-content-group-box .on").classList.toggle("on");
+    $(".tab-btn-box .on").classList.remove("on");
+    $(".tab-content-group-box .on").classList.remove("on");
 }
