4729f757ef322c7f410f1b305fe877ccc90a7fe3
woowatechcamp_11 <tech_rhh@woowahan.com>
2018-08-08 11:35:08 +0900

    feat: Add product searching api
    - Create product repository
    - Create search controller and service
    - Add search test
    - Add productList(String...) Method

--- src/test/java/codesquad/support/FixtureFactory.java
+++ src/test/java/codesquad/support/FixtureFactory.java
@@ -4,7 +4,9 @@ import codesquad.product.domain.BestProduct;
 import codesquad.product.domain.Product;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FixtureFactory {
     private static int lastProductIndex = 0;
@@ -13,9 +15,13 @@ public class FixtureFactory {
     public static List<Product> productList(int count) {
         List<Product> result = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            result.add(createProduct());
+            result.add(createProductBuilder().build());
         }
         return result;
+    }
+
+    public static List<Product> productList(String... titles) {
+        return Arrays.stream(titles).map(t -> createProductBuilder(t).build()).collect(Collectors.toList());
     }
 
     public static List<BestProduct> bestProductList(int count, int productSize) {
@@ -29,8 +35,12 @@ public class FixtureFactory {
         return result;
     }
 
-    private static Product createProduct() {
-        Product product = Product.builder()
+    private static Product.ProductBuilder createProductBuilder(String t) {
+        return createProductBuilder().title(t);
+    }
+
+    private static Product.ProductBuilder createProductBuilder() {
+        Product.ProductBuilder product = Product.builder()
                 .title("Product: " + lastProductIndex)
                 .category(null)
                 .discountPercent(50.0)
@@ -38,8 +48,7 @@ public class FixtureFactory {
                 .description("description")
                 .images(null)
                 .deliveryType("새벽배송")
-                .receiptableDays(null)
-                .build();
+                .receiptableDays(null);
 
         lastProductIndex++;
 
