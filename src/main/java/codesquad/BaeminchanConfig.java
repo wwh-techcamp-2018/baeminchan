122458075078c7d1320f1e29e79d724ecbd2ff26
woowatechcamp_11 <tech_rhh@woowahan.com>
2018-08-08 19:32:06 +0900

    feat: Apply Swagger2
    - Apply Swagger2 to "development" profile

--- src/main/java/codesquad/BaeminchanConfig.java
+++ src/main/java/codesquad/BaeminchanConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
@@ -64,10 +69,18 @@ public abstract class BaeminchanConfig implements WebMvcConfigurer {
 
     }
 
-
     @Configuration
+    @EnableSwagger2
     @Profile("development")
     static class BaeminchanDevConfig extends BaeminchanConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("codesquad"))
+                    .paths(PathSelectors.any())
+                    .build();
+        }
 
     }
 
